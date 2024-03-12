a6b575220a09a9d55bf4776d8668c2de580e7f8c
Ichihai1415 <projects3141.gif@gmail.com>
2024-03-12 22:01:27 +0900

    KNETASCII読み込みとgal変換　.tar.gz展開

---
+++ KmoniReproducer/DataClass.cs
@@ -0,0 +1,143 @@
+﻿using static KmoniReproducer.Program;
+namespace KmoniReproducer
+{
+    /// <summary>
+    /// 一地震での加速度データ
+    /// </summary>
+    public class Data
+    {
+        /// <summary>
+        /// K-NET ASCIIのファイル名を指定して<c>Data</c>を初期化します。
+        /// </summary>
+        /// <param name="fileNames">ファイル名</param>
+        public Data(string[] fileNames)
+        {
+            ConWrite($"{DateTime.Now:HH:MM:ss.ffff} 初期化中...");
+            string fileName1 = fileNames[0];
+            var fileText1 = File.ReadAllLines(fileName1);
+            for (var i = 0; i < 15; i++)
+                fileText1[i] = fileText1[i][18..];
+            OriginTime = DateTime.Parse(fileText1[0]);
+            HypoLat = double.Parse(fileText1[1]);
+            HypoLon = double.Parse(fileText1[2]);
+            ConWrite($"{DateTime.Now:HH:MM:ss.ffff} 読み込み中...");
+            KNETdatas = fileNames.Select(x => new KNET_ASCII(x)).ToArray();
+            ConWrite($"{DateTime.Now:HH:MM:ss.ffff} 読み込み完了  データ数:{KNETdatas.Length} メモリ:{GC.GetTotalMemory(true) / 1024d / 1024d:.00}MB", ConsoleColor.Blue);
+        }
+
+        /// <summary>
+        /// 発生時刻
+        /// </summary>
+        public DateTime OriginTime { get; set; } = DateTime.MinValue;
+
+        /// <summary>
+        /// 震源緯度
+        /// </summary>
+        public double HypoLat { get; set; } = 0;
+
+        /// <summary>
+        /// 震源経度
+        /// </summary>
+        public double HypoLon { get; set; } = 0;
+
+        /// <summary>
+        /// K-NET ASCIIフォーマットでのデータ
+        /// </summary>
+        public KNET_ASCII[]? KNETdatas { get; set; }
+
+        /// <summary>
+        /// K-NET ASCIIフォーマットでの一観測点のデータ
+        /// </summary>
+        public class KNET_ASCII
+        {
+            /// <summary>
+            /// ファイル名を指定して<c>KNET_ASCII</c>を初期化します。
+            /// </summary>
+            /// <param name="fileName">ファイル名</param>
+            public KNET_ASCII(string fileName)
+            {
+                //ConWrite(fileName, ConsoleColor.Green);
+                var fileText = File.ReadAllLines(fileName);
+                for (var i = 0; i < 15; i++)
+                    fileText[i] = fileText[i][18..];
+                StationCode = fileText[5];
+                StationLat = double.Parse(fileText[6]);
+                StationLon = double.Parse(fileText[7]);
+                RecordTime = DateTime.Parse(fileText[9]);
+                SamplingFreq = int.Parse(fileText[10].Replace("Hz", ""));
+                DataDir = fileText[12];
+                var scaleFs = fileText[13].Split("(gal)/").Select(double.Parse).ToArray();
+                ScaleFactor = scaleFs[0] / scaleFs[1];
+
+                var rawAccs = fileText.Skip(17).SelectMany(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries)).Select(int.Parse).ToArray();
+                fileText = null;
+                Accs = CorrectAccs(rawAccs, ScaleFactor);
+                rawAccs = null;
+            }
+
+            /// <summary>
+            /// 観測点コード
+            /// </summary>
+            public string StationCode { get; set; }
+
+            /// <summary>
+            /// 観測点緯度
+            /// </summary>
+            public double StationLat { get; set; }
+
+            /// <summary>
+            /// 観測点経度
+            /// </summary>
+            public double StationLon { get; set; }
+
+            /// <summary>
+            /// 記録開始時刻
+            /// </summary>
+            DateTime _RecordTime;
+
+            /// <summary>
+            /// 記録開始時刻(<b>指定時に自動で15秒引かれます。</b><see href="https://www.kyoshin.bosai.go.jp/kyoshin/man/knetform.html">強震データのK-NET ASCIIフォーマットについて</see>を参照)
+            /// </summary>
+            public DateTime RecordTime
+            {
+                get { return _RecordTime; }
+                set { _RecordTime = value.AddSeconds(-15); }//この時刻は強震計の遅延時間”１５秒”の効果を含んでいます。 したがって，真のスタート時刻を求めるためにはこの時刻から１５秒を引いて下さい。
+            }
+
+            /// <summary>
+            /// サンプリング周波数
+            /// </summary>
+            public int SamplingFreq { get; set; }
+
+            /// <summary>
+            /// 観測チャンネル
+            /// </summary>
+            public string DataDir { get; set; }
+
+            /// <summary>
+            /// スケールファクタ
+            /// </summary>
+            public double ScaleFactor { get; set; }
+
+            /// <summary>
+            /// 観測データ
+            /// </summary>
+            public double[] Accs { get; set; }
+
+            /// <summary>
+            /// 生の強震データをgalに変換します。
+            /// </summary>
+            /// <param name="rawAccs">生の強震データ</param>
+            /// <param name="scaleFacter">スケールファクタ</param>
+            /// <returns></returns>
+            public static double[] CorrectAccs(int[] rawAccs, double scaleFacter)
+            {
+                var ave = rawAccs.Average();
+                return rawAccs.Select(x => (x - ave) * scaleFacter).ToArray();
+            }
+
+
+
+        }
+    }
+}
--- KmoniReproducer/Program.cs
+++ KmoniReproducer/Program.cs
@@ -1,10 +1,126 @@
-﻿namespace KmoniReproducer
+﻿using ICSharpCode.SharpZipLib.Tar;
+using System.IO.Compression;
+
+namespace KmoniReproducer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var data =  GetDataFromKNETASCII();
+
+
+
+
+
+
+            Console.WriteLine();
+        }
+
+        public static void OpenTarGz(string dir)
+        {
+            ConWrite("展開中...");
+            while (true)
+            {
+                var targzFiles = Directory.EnumerateFiles(dir, "*.tar.gz", SearchOption.AllDirectories);
+                if (!targzFiles.Any())
+                    return;
+                foreach (string targzFile in targzFiles)
+                {
+                    ConWrite(targzFile,ConsoleColor.Green);
+                    using var tgzStream = File.OpenRead(targzFile);
+                    using var gzStream = new GZipStream(tgzStream, CompressionMode.Decompress);
+                    using var tarArchive = TarArchive.CreateInputTarArchive(gzStream, System.Text.Encoding.ASCII);
+                    tarArchive.ExtractContents(targzFile.Replace(".tar.gz",""));
+                }
+                Thread.Sleep(100);//待たないと使用中
+                foreach (string targzFile in targzFiles)
+                    File.Delete(targzFile);
+            }
+        }
+
+        public static Data? GetDataFromKNETASCII()
+        {
+            string dir = ConAsk("強震データがあるフォルダ名を入力してください。").Replace("\"", "");
+            var targzFiles = Directory.EnumerateFiles(dir, "*.tar.gz", SearchOption.AllDirectories).ToArray();
+            if (targzFiles.Length > 0)
+            {
+                bool ok = ConAsk(".tar.gzファイルが見つかりました。展開しますか？(y/n)") == "y";
+                if (ok)
+                    OpenTarGz(dir);
+            }
+            ConWrite($"{DateTime.Now:HH:MM:ss.ffff} 取得中...");
+            var files = Directory.EnumerateFiles(dir, "*", SearchOption.AllDirectories).Where(f => f.EndsWith(".NS") || f.EndsWith(".EW") || f.EndsWith(".UD") || f.EndsWith(".NS2") || f.EndsWith(".EW2") || f.EndsWith(".UD2")).ToArray();
+            if (files.Length == 0)
+            {
+                ConWrite("見つかりませんでした。");
+                return null;
+            }
+            return new Data(files);
+        }
+
+
+
+
+
+
+        public static string ConAsk(string message, bool allowNull = false)
+        {
+            ConWrite(message);
+        retry:
+            string? ans = Console.ReadLine();
+            if (allowNull)
+                return ans ?? "";
+            else if (string.IsNullOrEmpty(ans))
+            {
+                ConWrite("値を入力してください。" + message);
+                goto retry;
+            }
+            else
+                return ans;
+        }
+
+
+
+        /// <summary>
+        /// コンソールのデフォルトの色
+        /// </summary>
+        public static readonly ConsoleColor defaultColor = Console.ForegroundColor;
+
+        /// <summary>
+        /// コンソールにデフォルトの色で出力します。
+        /// </summary>
+        /// <param name="text">出力するテキスト</param>
+        /// <param name="withLine">改行するか</param>
+        public static void ConWrite(string text, bool withLine = true)
+        {
+            ConWrite(text, defaultColor, withLine);
+        }
+
+        /// <summary>
+        /// 例外のテキストを赤色で出力します。
+        /// </summary>
+        /// <param name="loc">場所([ConWrite]など)</param>
+        /// <param name="ex">出力する例外</param>
+        public static void ConWrite(string loc, Exception ex)
+        {
+            ConWrite(loc + ex.ToString(), ConsoleColor.Red);
+        }
+
+        /// <summary>
+        /// コンソールに色付きで出力します。色は変わったままとなります。
+        /// </summary>
+        /// <param name="text">出力するテキスト</param>
+        /// <param name="color">表示する色</param>
+        /// <param name="withLine">改行するか</param>
+        public static void ConWrite(string text, ConsoleColor color, bool withLine = true)
+        {
+            Console.ForegroundColor = color;
+            //Console.Write(DateTime.Now.ToString("HH:mm:ss.ffff "));
+            if (withLine)
+                Console.WriteLine(text);
+            else
+                Console.Write(text);
         }
     }
 }
